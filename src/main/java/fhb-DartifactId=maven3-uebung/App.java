bc8152fd55cd2e6f0b1ee3d85d280dcef6284ba5
Ehab-Fadl <fadl@th-brandenburg.de>
2019-10-20 18:34:34 +0200

    Erster Commit

---
+++ src/main/java/fhb-DartifactId=maven3-uebung/App.java
@@ -0,0 +1,13 @@
+package fhb-DartifactId=maven3-uebung;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/fhb-DartifactId=maven3-uebung/AppTest.java
@@ -0,0 +1,20 @@
+package fhb-DartifactId=maven3-uebung;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
